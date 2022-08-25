3866323f3b9b920dee2d2096f43105811da483fd
ramadan82 <ramadan.isa@gmail.com>
2022-08-25 15:29:37 +0100

    modified pages

--- src/components/pages/SignUp.js
+++ src/components/pages/SignUp.js
@@ -1,103 +1,103 @@
-// import React from "react";
-// import "./Login.css";
-// import {
-//   MDBBtn,
-//   MDBContainer,
-//   MDBRow,
-//   MDBCol,
-//   MDBInput,
-// } from "mdb-react-ui-kit";
-// import { Link } from "react-router-dom";
+import React from "react";
+import "./Login.css";
+import {
+  MDBBtn,
+  MDBContainer,
+  MDBRow,
+  MDBCol,
+  MDBInput,
+} from "mdb-react-ui-kit";
+import { Link } from "react-router-dom";
 
-// function Login() {
-//   return (
-//     <MDBContainer className="my-5 gradient-form">
-//       <MDBRow style={{ zIndex: 3, position: "relative" }}>
-//         <MDBCol col="6" className="mb-5" style={{ marginRight: "50px" }}>
-//           <div className="d-flex flex-column ms-5">
-//             <div className="text-center">
-//               <img
-//                 src="images/bigdataconsult.png"
-//                 style={{ width: "185px" }}
-//                 alt="logo"
-//               />
-//               <h4 className="mt-1 mb-5 pb-1">
-//                 Consult <span style={{ color: "darkred" }}>Limited</span>
-//               </h4>
-//             </div>
+function Login() {
+  return (
+    <MDBContainer className="my-5 gradient-form">
+      <MDBRow style={{ zIndex: 3, position: "relative" }}>
+        <MDBCol col="6" className="mb-5" style={{ marginRight: "50px" }}>
+          <div className="d-flex flex-column ms-5">
+            <div className="text-center">
+              <img
+                src="images/bigdataconsult.png"
+                style={{ width: "185px" }}
+                alt="logo"
+              />
+              <h4 className="mt-1 mb-5 pb-1">
+                Consult <span style={{ color: "darkred" }}>Limited</span>
+              </h4>
+            </div>
 
-//             <p>Register Now</p>
+            <p>Register Now</p>
 
-//             <MDBCol col="6">
-//               <MDBInput
-//                 wrapperClass="mb-4"
-//                 label="First Name"
-//                 id="form1"
-//                 type="text"
-//               />
-//             </MDBCol>
-//             <MDBCol col="6">
-//               <MDBInput
-//                 wrapperClass="mb-4"
-//                 label="Last Name"
-//                 id="form1"
-//                 type="text"
-//               />
-//             </MDBCol>
+            <MDBCol col="6">
+              <MDBInput
+                wrapperClass="mb-4"
+                label="First Name"
+                id="form1"
+                type="text"
+              />
+            </MDBCol>
+            <MDBCol col="6">
+              <MDBInput
+                wrapperClass="mb-4"
+                label="Last Name"
+                id="form1"
+                type="text"
+              />
+            </MDBCol>
 
-//             <MDBInput
-//               wrapperClass="mb-4"
-//               label="Email address"
-//               id="form1"
-//               type="email"
-//             />
-//             <MDBInput
-//               wrapperClass="mb-4"
-//               label="Password"
-//               id="form2"
-//               type="password"
-//             />
-//             <MDBInput
-//               wrapperClass="mb-4"
-//               label="Confirm Password"
-//               id="form2"
-//               type="password"
-//             />
+            <MDBInput
+              wrapperClass="mb-4"
+              label="Email address"
+              id="form1"
+              type="email"
+            />
+            <MDBInput
+              wrapperClass="mb-4"
+              label="Password"
+              id="form2"
+              type="password"
+            />
+            <MDBInput
+              wrapperClass="mb-4"
+              label="Confirm Password"
+              id="form2"
+              type="password"
+            />
 
-//             <div className="text-center pt-1 mb-5 pb-1">
-//               <MDBBtn
-//                 className="mb-4 w-100 "
-//                 style={{ backgroundColor: "darkred" }}
-//               >
-//                 Register
-//               </MDBBtn>
-//             </div>
+            <div className="text-center pt-1 mb-5 pb-1">
+              <MDBBtn
+                className="mb-4 w-100 "
+                style={{ backgroundColor: "darkred" }}
+              >
+                Register
+              </MDBBtn>
+            </div>
 
-//             <div className="d-flex flex-row align-items-center justify-content-center pb-4 mb-4">
-//               <p className="mb-0">Already have an account?</p>
-//               <MDBBtn outline className="mx-2" style={{ color: "darkred" }}>
-//                 <Link to="/login">Login Now</Link>
-//               </MDBBtn>
-//             </div>
-//           </div>
-//         </MDBCol>
+            <div className="d-flex flex-row align-items-center justify-content-center pb-4 mb-4">
+              <p className="mb-0">Already have an account?</p>
+              <MDBBtn outline className="mx-2" style={{ color: "darkred" }}>
+                <Link to="/login">Login Now</Link>
+              </MDBBtn>
+            </div>
+          </div>
+        </MDBCol>
 
-//         <MDBCol col="6" className="mb-5" style={{ backgroundColor: "darkred" }}>
-//           <div className="d-flex flex-column  justify-content-center gradient-custom-2 h-100 mb-4">
-//             <div className="text-white px-3 py-4 p-md-5 mx-md-4">
-//               <h4 class="mb-4">We are more than just a company</h4>
-//               <p class="small mb-0">
-//                 Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do
-//                 eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
-//                 enim ad minim veniam, quis nostrud exercitation ullamco laboris
-//                 nisi ut aliquip ex ea commodo consequat.
-//               </p>
-//             </div>
-//           </div>
-//         </MDBCol>
-//       </MDBRow>
-//     </MDBContainer>
-//   );
-// }
+        <MDBCol col="6" className="mb-5" style={{ backgroundColor: "darkred" }}>
+          <div className="d-flex flex-column  justify-content-center gradient-custom-2 h-100 mb-4">
+            <div className="text-white px-3 py-4 p-md-5 mx-md-4">
+              <h4 class="mb-4">We are more than just a company</h4>
+              <p class="small mb-0">
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do
+                eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+                enim ad minim veniam, quis nostrud exercitation ullamco laboris
+                nisi ut aliquip ex ea commodo consequat.
+              </p>
+            </div>
+          </div>
+        </MDBCol>
+      </MDBRow>
+    </MDBContainer>
+  );
+}
 
-// export default Login;
+export default Login;
